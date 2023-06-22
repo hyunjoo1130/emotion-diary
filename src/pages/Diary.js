c385022535887aa5f760bb66b7b37cce3dddce8c
hyun joo oh <58460131+hyunjoo1130@users.noreply.github.com>
2023-06-22 15:54:43 +0900

    페이지 라우팅 1 - React Router 기본

---
+++ src/pages/Diary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Diary = () => {
+  return (
+    <div>
+      <h1>이곳은 Diary 입니다.</h1>
+    </div>
+  );
+};
+
+export default Diary;
---
+++ src/pages/Edit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Edit = () => {
+  return (
+    <div>
+      <h1>이곳은 Edit 입니다.</h1>
+    </div>
+  );
+};
+
+export default Edit;
